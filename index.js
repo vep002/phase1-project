cdca513c8b6e5df5fde35adbd0944efcd3ca9321
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-05-12 15:10:58 -0400

    refactoring

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 //GLOBAL STUFF
 let moviesList = document.querySelector('div#list')
 let movieForm = document.querySelector('form')
@@ -15,30 +14,34 @@ let genreSelect = document.querySelector('select#genre');
     let comedy = genreSelect.querySelector('option.genre4')
 let filteredMoviesList = document.querySelector('div#new-list.filtered-movies-list')
 
-    // let dropDownContent = dropDown.querySelector('div.dropdown-content')
+////////GLOBAL FUNCTION CALLS
+getMovies()
+submitNewMovie()
+//////////////
 
 
 //Get movies from database
-
 function getMovies() {
     fetch('http://localhost:3000/movies')
     .then(resp => resp.json())
     .then((moviesArray)=> {displayMovies(moviesArray)})
 }
 
-//function to display movies to the DOM
+//function unpacks movies from database
 function displayMovies (moviesArray) {
-moviesArray.forEach(movieObj => {
-    //create a span element for each movie, which is appended to the overall movie div
+    moviesArray.forEach((movieObj) => {
+    movieDetails(moviesArray, movieObj)})  
+}
+
+//displays movieObjects on the DOM
+function movieDetails(moviesArray, movieObj){
     let movieSpan = document.createElement('span')
     moviesList.append(movieSpan)
-
     //create elements for each movie detail
     let movieTitle = document.createElement('h1')
     movieSpan.append(movieTitle)
     movieTitle.innerText = movieObj.title
     
-
     let movieDirector=document.createElement('p')
     movieSpan.append(movieDirector)
     movieDirector.innerText= `Director: ${movieObj.director}`
@@ -73,30 +76,18 @@ moviesArray.forEach(movieObj => {
     deleteButton.innerText = "Delete"
     deleteButton.id = movieObj.id
     deleteMovie(deleteButton)
-
-    //movieDetails(moviesArray)
-});
-
-    
-
 }
-getMovies()
-submitNewMovie()
-//fill in movie details
-//function movieDetails (moviesArray, element, movieTitle) {}
     
 //User can create a movie:
 function submitNewMovie () {
   
     movieForm.addEventListener('submit', (e) => {
-       
-
         let newDirector = formDirector.value 
         let newTitle = formTitle.value  
         let newYear = formYear.value
         let newGenre = formGenre.value 
         let newReview = formReview.value
-        let newPoster = formPoster.value 
+        // let newPoster = formPoster.value 
     //persist to back end
         fetch('http://localhost:3000/movies', {
       method: "POST",
@@ -109,53 +100,16 @@ function submitNewMovie () {
           year: newYear,
           genre: newGenre,
           review: newReview,
-          thumbnail: newPoster
+        //   thumbnail: newPoster
       }),
-    })
+      
+    }) 
         .then((r) => r.json())
-        .then((element) => function (element) {
-                //create a span element for each movie, which is appended to the overall movie div
-                let movieSpan = document.createElement('span')
-                moviesList.append(movieSpan)
-            
-                //create elements for each movie detail
-                let movieTitle = document.createElement('h1')
-                movieSpan.append(movieTitle)
-                movieTitle.innerText = element.title
-                
-            
-                let movieDirector=document.createElement('p')
-                movieSpan.append(movieDirector)
-                movieDirector.innerText= `Director: ${element.director}`
-            
-                let movieYear= document.createElement('p')
-                movieSpan.append(movieYear)
-                movieYear.innerText = `Year Released: ${element.year}` 
-                
-                let movieGenre = document.createElement ('p')
-                movieSpan.append(movieGenre)
-                movieGenre.innerText = `Genre: ${element.genre}` 
-            
-                // let moviePoster = document.createElement('img')
-                // movieSpan.append(moviePoster)
-                // moviePoster.src = element.thumbnail
-            
-                // let movieTrailer = document.createElement('iframe')
-                // movieSpan.append(movieTrailer)
-                // movieTrailer.src = element.preview
-            
-                // let movieRating = document.createElement('p')
-                // movieSpan.append(movieRating)
-                // movieRating.innerText = element.rating 
-            
-                let movieReview = document.createElement('p')
-                movieSpan.append(movieReview)
-                movieReview.innerText = `Review: ${element.review}`;
-    
-    
-    })
-    })}
+        .then((movieObj) => movieDetails(movieObj))
+        debugger;
 
+})           
+}
 function filterMovies(moviesArray) {
     genreSelect.onchange = function(e) {
         // console.log('hi')
@@ -218,3 +172,48 @@ function deleteFilteredMovie(deleteButton) {
         newSPan.remove()
     })
 }
+
+
+
+
+// debugger
+                // //create a span element for each movie, which is appended to the overall movie div
+                // let newMovieSpan = document.createElement('span')
+                // moviesList.append(newMovieSpan)
+            
+                // //create elements for each movie detail
+                // let newMovieTitle = document.createElement('h1')
+                // newMovieSpan.append(newMovieTitle)
+                // newMovieTitle.innerText = element.title
+                
+            
+                // let newMovieDirector=document.createElement('p')
+                // newMovieSpan.append(newMovieDirector)
+                // newMovieDirector.innerText= `Director: ${element.director}`
+            
+                // let newMovieYear= document.createElement('p')
+                // newMovieSpan.append(newMovieYear)
+                // newMovieYear.innerText = `Year Released: ${element.year}` 
+                
+                // let newMovieGenre = document.createElement ('p')
+                // newMovieSpan.append(newMovieGenre)
+                // newMovieGenre.innerText = `Genre: ${element.genre}` 
+            
+                // // let newMoviePoster = document.createElement('img')
+                // // newMovieSpan.append(newMoviePoster)
+                // // newMoviePoster.src = element.thumbnail
+            
+                // // let newMovieTrailer = document.createElement('iframe')
+                // // newMovieSpan.append(newMovieTrailer)
+                // // newMovieTrailer.src = element.preview
+            
+                // // let newMovieRating = document.createElement('p')
+                // // newMovieSpan.append(newMovieRating)
+                // // newMovieRating.innerText = element.rating 
+            
+                // let newMovieReview = document.createElement('p')
+                // newMovieSpan.append(newMovieReview)
+                // newMovieReview.innerText = `Review: ${element.review}`;
+//         })
+//     })
+ 
