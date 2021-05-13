9b62aa493d1d94b3b9ea9945f6a126367715399c
Victoria <vep002@gmail.com>
2021-05-13 07:06:46 -0500

    get videos

--- index.js
+++ index.js
@@ -1,85 +1,105 @@
-
 //GLOBAL STUFF
 let moviesList = document.querySelector('div#list')
 let movieForm = document.querySelector('form')
-let formTitle = document.querySelector('input#title')
-let formDirector = document.querySelector('input#director')
-let formYear = document.querySelector('input#year')
-let formGenre = document.querySelector('input#genre')
-let formReview = document.querySelector('input#review')
-let formPoster = document.queryCommandEnabled('input#poster')
-//let dropDown = document.querySelector()
+    let formTitle = movieForm.querySelector('input#title')
+    let formDirector = movieForm.querySelector('input#director')
+    let formYear = movieForm.querySelector('input#year')
+    let formGenre = movieForm.querySelector('input#genre')
+    let formReview = movieForm.querySelector('input#review')
+// let dropDown = document.querySelector('div.dropdown')
+// let genreSelect = document.querySelector('select#genre');
+//     let horror = genreSelect.querySelector('option.genre1')
+//     let drama = genreSelect.querySelector('option.genre2')
+//     let action = genreSelect.querySelector('option.genre3')
+//     let comedy = genreSelect.querySelector('option.genre4')
+// let filteredMoviesList = document.querySelector('div#new-list.filtered-movies-list')
+
+////////GLOBAL FUNCTION CALLS
+getMovies()
+submitNewMovie()
+//////////////
 
 
 //Get movies from database
-
 function getMovies() {
     fetch('http://localhost:3000/movies')
     .then(resp => resp.json())
     .then((moviesArray)=> {displayMovies(moviesArray)})
 }
 
-//function to display movies to the DOM
+//function unpacks movies from database
 function displayMovies (moviesArray) {
-moviesArray.forEach(element => {
-    //create a span element for each movie, which is appended to the overall movie div
-    let movieSpan = document.createElement('span')
-    moviesList.append(movieSpan)
+    moviesArray.forEach((movieObj) => {
+    movieDetails(moviesArray, movieObj)}) 
+}
 
+//displays movieObjects on the DOM
+function movieDetails(moviesArray, movieObj){
+    let movieSpan = document.createElement('span')
+    movieSpan.id = movieObj.id
+    moviesList.append(movieSpan)
     //create elements for each movie detail
     let movieTitle = document.createElement('h1')
     movieSpan.append(movieTitle)
-    movieTitle.innerText = element.title
+    movieTitle.innerText = movieObj.title
     
-
     let movieDirector=document.createElement('p')
     movieSpan.append(movieDirector)
-    movieDirector.innerText= `Director: ${element.director}`
+    movieDirector.innerText= `Director: ${movieObj.director}`
 
     let movieYear= document.createElement('p')
     movieSpan.append(movieYear)
-    movieYear.innerText = `Year Released: ${element.year}` 
+    movieYear.innerText = movieObj.year 
     
     let movieGenre = document.createElement ('p')
     movieSpan.append(movieGenre)
-    movieGenre.innerText = `Genre: ${element.genre}` 
+    movieGenre.innerText = movieObj.genre
+    //filterMovies(moviesArray)
 
     let moviePoster = document.createElement('img')
     movieSpan.append(moviePoster)
-    moviePoster.src = element.thumbnail
+    moviePoster.src = movieObj.thumbnail
 
-    // let movieTrailer = document.createElement('iframe')
-    // movieSpan.append(movieTrailer)
-    // movieTrailer.src = element.preview
-
+    function getVideos (movieObj) {
+        fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=raising+arizona&key=`)
+        .then(resp => resp.json())
+        .then ((resp)=> {
+            let videoId = resp.items[0].id.videoId
+            let movieTrailer = document.createElement('iframe')
+            movieTrailer.src=`https://www.youtube.com/embed/${videoId}`
+            movieSpan.append(movieTrailer)
+            // let example = document.createElement('iframe')
+            // example.src = 
+            // moviesList.append(example)
+        })
+        }
+        getVideos()
     // let movieRating = document.createElement('p')
     // movieSpan.append(movieRating)
     // movieRating.innerText = element.rating 
 
     let movieReview = document.createElement('p')
     movieSpan.append(movieReview)
-    movieReview.innerText = `Review: ${element.review}`
+    movieReview.innerText = `Review: ${movieObj.review}`
 
-    //movieDetails(moviesArray)
-});
+    let deleteButton = document.createElement('button')
+    movieSpan.append(deleteButton)
+    deleteButton.innerText = "Delete"
+    deleteButton.id = movieObj.id
+    //deleteMovie(deleteButton, movieSpan)
+     
 }
-getMovies()
-submitNewMovie()
-//fill in movie details
-//function movieDetails (moviesArray, element, movieTitle) {}
     
 //User can create a movie:
 function submitNewMovie () {
   
     movieForm.addEventListener('submit', (e) => {
-       
-
         let newDirector = formDirector.value 
         let newTitle = formTitle.value  
         let newYear = formYear.value
         let newGenre = formGenre.value 
         let newReview = formReview.value
-        let newPoster = formPoster.value 
+        // let newPoster = formPoster.value 
     //persist to back end
         fetch('http://localhost:3000/movies', {
       method: "POST",
@@ -92,49 +112,93 @@ function submitNewMovie () {
           year: newYear,
           genre: newGenre,
           review: newReview,
-          thumbnail: newPoster
+        //   thumbnail: newPoster
       }),
-    })
+      
+    }) 
         .then((r) => r.json())
-        .then((element) => function (element) {
-                //create a span element for each movie, which is appended to the overall movie div
-                let movieSpan = document.createElement('span')
-                moviesList.append(movieSpan)
-            
-                //create elements for each movie detail
-                let movieTitle = document.createElement('h1')
-                movieSpan.append(movieTitle)
-                movieTitle.innerText = element.title
+        .then((movieObj) => movieDetails(movieObj))
+
+})           
+}
+// function filterMovies(moviesArray) {
+//     genreSelect.onchange = function(e) {
+//         // console.log('hi')
+//       let  allMovies = document.querySelector("div#list.movies-list")
+//       allMovies.remove()
+        
+//       moviesArray.filter(function(movieObj) {
+//            if (movieObj.genre === genreSelect.value){
+//             //    console.log('hello!')
+//             //    debugger;
+//             let movieSpan = document.createElement('span')
+//             movieSpan.id = movieObj.id
+//             filteredMoviesList.append(movieSpan)
+
+//             let movieTitle = document.createElement('h1')
+//              movieSpan.append(movieTitle)
+//              movieTitle.innerText = movieObj.title
+//              let movieDirector=document.createElement('p')
+//                 movieSpan.append(movieDirector)
+//                 movieDirector.innerText=movieObj.director
+
+//                 let movieYear= document.createElement('p')
+//                 movieSpan.append(movieYear)
+//                 movieYear.innerText = movieObj.year 
                 
-            
-                let movieDirector=document.createElement('p')
-                movieSpan.append(movieDirector)
-                movieDirector.innerText= `Director: ${element.director}`
-            
-                let movieYear= document.createElement('p')
-                movieSpan.append(movieYear)
-                movieYear.innerText = `Year Released: ${element.year}` 
-                
-                let movieGenre = document.createElement ('p')
-                movieSpan.append(movieGenre)
-                movieGenre.innerText = `Genre: ${element.genre}` 
-            
-                // let moviePoster = document.createElement('img')
-                // movieSpan.append(moviePoster)
-                // moviePoster.src = element.thumbnail
-            
-                // let movieTrailer = document.createElement('iframe')
-                // movieSpan.append(movieTrailer)
-                // movieTrailer.src = element.preview
-            
-                // let movieRating = document.createElement('p')
-                // movieSpan.append(movieRating)
-                // movieRating.innerText = element.rating 
-            
-                let movieReview = document.createElement('p')
-                movieSpan.append(movieReview)
-                movieReview.innerText = `Review: ${element.review}`;
-    
-    
-    })
-    })}
+//                 let movieGenre = document.createElement ('p')
+//                 movieSpan.append(movieGenre)
+//                 movieGenre.innerText = movieObj.genre
+
+//                 let movieReview = document.createElement('p')
+//                 movieSpan.append(movieReview)
+//                 movieReview.innerText = movieObj.review
+               
+//                 let moviePoster = document.createElement('img')
+//                 movieSpan.append(moviePoster)
+//                 moviePoster.src = movieObj.thumbnail
+
+//                 let deleteButton = document.createElement('button')
+//                 movieSpan.append(deleteButton)
+//                 deleteButton.innerText = "Delete"
+//                 deleteButton.id = movieObj.id
+//                 deleteFilteredMovie(deleteButton)
+//            }
+//         })
+//     }
+// }
+// function deleteMovie(deleteButton, movieSpan) {
+//     deleteButton.addEventListener('click', (e) => {
+//         fetch(`http://localhost:3000/movies/${e.target.id}`, {
+//   method: "DELETE",
+// })
+//   .then((r) => r.json())
+//   .then((movieObj) => {
+//     if (movieSpan.id === e.target.id) {
+//         movieSpan.remove()
+//     }
+//   }) 
+
+//     })
+// }
+// function deleteFilteredMovie(deleteButton) {
+//     deleteButton.addEventListener('click',(e) => {
+//         let newSPan = document.querySelector('div#new-list.filtered-movies-list span')
+//         newSPan.remove()
+//     })
+// }
+
+ 
+
+
+
+// function getVideos(search){
+//     const API_KEY = 
+
+//     fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${search}&key=${API_KEY }`)
+//     .then(res => res.json())
+//     .then(videos =>{
+//         videos.items.forEach(video => buildAside(video))
+//         buildVideo(videos.items[0])
+//     })
+// }
